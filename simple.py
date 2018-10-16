58a9e9fa632c80794b0b89bbfc61a52c099292e6
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-10-16 15:01:43 +0530

    properly lint the code

--- simple.py
+++ simple.py
@@ -1,7 +1,7 @@
 import hashlib as hasher
 import datetime as date
-from src.block import Block 
-from src.chain import blockchain, add_block, previous_block 
+from src.block import Block
+from src.chain import blockchain, add_block, previous_block
 
 # How many blocks do we need for this chain
 # after the genesis block
@@ -14,5 +14,5 @@ for i in range(0, num_of_blocks_to_add):
     previous_block = block_to_add
 
     # Print to console the newly created block
-    print ("Block #{} has been added to blockchain!".format(block_to_add.index))
-    print ("Hash: {}\n".format(block_to_add.hash))
+    print("Block #{} has been added to blockchain!".format(block_to_add.index))
+    print("Hash: {}\n".format(block_to_add.hash))
